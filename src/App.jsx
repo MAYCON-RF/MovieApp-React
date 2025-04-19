6c1c70b09c3dad28391692d1fe0703435bb94cef
Maycon Rodrigues <maycon.rf@outlook.com>
2025-04-19 20:26:29 -0300

    Criação da aba Home

--- src/App.jsx
+++ src/App.jsx
@@ -9,22 +9,22 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://vite.dev" target="_blank" className="logo-wrapper">
+          <img src={viteLogo} className="logo spin-3d" alt="Vite logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+
+      <h1>Bem Vindo ao Movie App React</h1>
+
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+  <button onClick={() => window.location.href = '/home'}>
+    Entrar no app
+  </button>
+  <p>
+    Bem-vindo! Clique para continuar para a próxima página.
+  </p>
+</div>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
