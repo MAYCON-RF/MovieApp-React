124e7aa3e7f9d5ef285fdd1b37e386cc9a4ad07c
Maycon Rodrigues <maycon.rf@outlook.com>
2025-04-19 20:51:57 -0300

    Adicionado pagina homem com React Rout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
+import { Link } from 'react-router-dom'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
@@ -14,16 +14,16 @@ function App() {
         </a>
       </div>
 
-      <h1>Bem Vindo ao Movie App React</h1>
+      <h1>Bem Vindo ao Movie App React!</h1>
 
       <div className="card">
-  <button onClick={() => window.location.href = '/home'}>
-    Entrar no app
-  </button>
-  <p>
-    Bem-vindo! Clique para continuar para a próxima página.
-  </p>
-</div>
+        <Link to="/home">
+          <button>Entrar no app</button>
+        </Link>
+        <p>
+          Clique no botão acima para acessar a próxima página.
+        </p>
+      </div>
 
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
---
+++ src/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+      <div style={{ textAlign: 'center', marginTop: '4rem' }}>
+        <h1>Bem-vindo à Home</h1>
+        <p>Essa é a próxima página do app.</p>
+      </div>
+    )
+  }
